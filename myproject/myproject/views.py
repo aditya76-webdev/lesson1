301b29f3dfd77e97a7402911c70f061354183338
Aditya Jain <adityajain77356@gmail.com>
2025-06-20 11:54:37 +0530

    django-project-progress

---
+++ myproject/myproject/views.py
@@ -0,0 +1,11 @@
+# from django.http import HttpResponse
+
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse("Hello world! I m home. ")
+    return render(request,'home.html')
+
+def about(request):
+    # return HttpResponse("My about Page !")
+    return render(request,'about.html')
